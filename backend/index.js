3bc2e482a2f4f5bfce67cf5d049d6af077ce0b7b
Sahil Khan <95959159+sahilkhan786000@users.noreply.github.com>
2023-12-19 23:54:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,22 +5,22 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
 
-import corsOptions from './corsOptions.js'
+// import corsOptions from './corsOptions.js'
 const _dirname = path.resolve()
 const app = express();
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json({extended:true})); 
 app.use('/', routes);
 
-// app.use(express.static(path.join(_dirname, "./me/build")))
+app.use(express.static(path.join(_dirname, "./me/build")))
 
-// app.get('*', function(_,res){
-//   res.sendFile(path.join(__dirname, "./me/build/index.html", function(err){
-//     res.status(500).send(err);
-//   }))
-// })
+app.get('*', function(_,res){
+  res.sendFile(path.join(__dirname, "./me/build/index.html", function(err){
+    res.status(500).send(err);
+  }))
+})
 const PORT =process.env.PORT || 8080 ;
 // Connect to database before starting server
 mongoDB();
